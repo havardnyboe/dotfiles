0798f02ad8aad78ea2fac20a802411c50cb00b43
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2023-12-17 14:10:35 +0100

    bytta theme

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -2,8 +2,8 @@
 local M = {}
 
 M.ui = {
-  theme = "doomchad",
-  theme_toggle = { "doomchad", "one_light" },
+  theme = "github_dark",
+  theme_toggle = { "github_dark", "one_light" },
 }
 
 return M
